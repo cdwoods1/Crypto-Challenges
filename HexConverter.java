58811d1b472c4170a3371cd823ffaa767382670e
cdwoods1 <39446634+cdwoods1@users.noreply.github.com>
2019-08-08 17:22:18 -0600

    Update HexConverter.java

--- HexConverter.java
+++ HexConverter.java
@@ -60,10 +60,4 @@ public class HexConverter {
         }
         return byteArrayToHexConverter(xorArray);
     }
-    public static void main(String[] args) {
-        System.out.println(xorProducer("1c0111001f010100061a024b53535009181c",
-                "686974207468652062756c6c277320657965"));
-    }
-
-
 }
